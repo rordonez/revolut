179ece43700d16b039a75188cb1bce3ea5b15d0d
Rafael Ordoñez <rafaelordonezvivar@gmail.com>
2015-09-21 23:09:33 +0100

    Initial commit for Revolut Exercise

---
+++ src/main/java/revolut/RevolutApplication.java
@@ -0,0 +1,12 @@
+package revolut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevolutApplication.class, args);
+    }
+}
